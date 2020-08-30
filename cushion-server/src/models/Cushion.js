693563bdcf0fe9b69333c59e18f7660a5216a404
huhsame <brave.huhsame@gmail.com>
2020-08-30 15:47:57 +0900

    Cushion Model 스키마 수정

--- cushion-server/src/models/Cushion.js
+++ cushion-server/src/models/Cushion.js
@@ -2,8 +2,8 @@ const mongoose = require('mongoose');
 const PointSchema = require('./Point');
 
 const cushionSchema = new mongoose.Schema({
-  id: 'number',
-  time: 'Date',
+  id: Number,
+  time: Date,
   points: [PointSchema]
 });
 mongoose.model('Cushion', cushionSchema);
